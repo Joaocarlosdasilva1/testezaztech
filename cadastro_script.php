45ee108960137348ee262a9bf3574cf8d3d08803
João Carlos <carlosjoao489ilha@outlook.com>
2021-10-06 20:42:35 -0300

    index, conexao, cadastroscript
    criado index, conexão com o banco e inicio do cadastro de script

---
+++ cadastro_script.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+    <title>Cadastro de Usuários</title>
+</head>
+<body>
+    <div class="container">
+        <div class ="row">
+            <?php
+                $nome            = $_POST['nome'];
+                $telefone        = $_POST['telefone'];
+                $email           = $_POST['email'];
+                $data_nascimento = $_POST['data_nascimento'];
+                
+                $sql = "INSERT INTO `testezaztech`( `nome`, `telefone`, `email`, `data_nascimento`) VALUES ('$nome','$telefone','$email','$data_nascimento')";
+
+                mysqli_query($)
+            ?>
+        </div>
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.min.js" integrity="sha384-PsUw7Xwds7x08Ew3exXhqzbhuEYmA2xnwc8BuD6SEr+UmEHlX8/MCltYEodzWA4u" crossorigin="anonymous"></script>
+</body>
+</html>
